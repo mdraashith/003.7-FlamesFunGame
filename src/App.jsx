c759035cb8798515651b5e479a4f1426ae5c5568
mdraashith <mdraashith653@gmail.com>
2026-05-26 15:21:37 +0530

    game

--- src/App.jsx
+++ src/App.jsx
@@ -160,7 +160,7 @@ function App() {
                 sm: "25rem"
               },
               minHeight: {
-                xs: "11rem",
+                xs: "9rem",
                 md: "15rem"
               },
               border:"1px solid black",
